55098b22070f24dce9066d0c3c6716bce6c6dc28
berorev <berorev@gmail.com>
2014-02-20 16:44:08 +0900

    study - block_init

---
+++ block_init.rb
@@ -0,0 +1,14 @@
+class Mail
+  def initialize
+    yield(self)
+    puts @from_email
+  end
+  
+  def from(from_email)
+    @from_email = from_email
+  end
+end
+
+Mail.new do |m|
+  m.from 'from@gmail.com'
+end
